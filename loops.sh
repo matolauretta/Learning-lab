dfdfd1b890508f64dc470ac8be5c769b4f946b43
Matias Lauretta <matias@hellopearl.com>
2025-07-05 13:45:32 -0300

    loops exapmles

---
+++ loops.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script demonstrates the use of loops in bash
+
+primary_colors="red blue yellow"
+index=1
+
+for word in $primary_colors; do
+    counter=0
+    length=${#word}
+
+    while [ $counter -lt $length ]; do
+        ((counter++))
+    done
+
+    echo "The $index primary color $word has $counter letters."
+    ((index++))
+done
+
+echo "Enter a list of your favorite colors (space-separated):"
+read -a colors
+
+for word in $colors; do
+    length=${#word}
+    number=1
+    until [ "$number" -eq $length ]; do
+        ((number++))
+    done
+     echo "The number of colors you entered is: $number"
+done
